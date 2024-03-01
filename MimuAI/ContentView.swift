29aa8bb4b5dd7cbef1745f9717a646fcd8eff292
ducduyn31 <ducduy.ababat@gmail.com>
2024-03-01 23:16:09 +1100

    Initial Commit

---
+++ MimuAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MimuAI
+//
+//  Created by Duy Nguyen on 1/3/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MimuAI/MimuAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MimuAIApp.swift
+//  MimuAI
+//
+//  Created by Duy Nguyen on 1/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct MimuAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
